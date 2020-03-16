f7310f4102a1c4a42fb0c71ddbf332768d2dde42
yesmij <yesmij@gmail.com>
2020-03-16 14:06:15 +0900

    lombok & test Code

---
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokTest() {
+        String name= "test";
+        int amount = 1000;
+
+        HelloResponseDto helloResponseDto = new HelloResponseDto(name, amount);
+
+        assertThat(helloResponseDto.getName()).isEqualTo(name);
+        assertThat(helloResponseDto.getAmount()).isEqualTo(amount);
+    }
+
+}
